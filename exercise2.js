86cc7d3180dc566311f89302a63c7b317b0fe5a8
Yin.He <heyin.sg@gmail.com>
2021-09-27 14:39:45 +0800

    completed exercise 2 using query Selector

---
+++ exercise2.js
@@ -0,0 +1,8 @@
+document.querySelector("#fun-fact").addEventListener("click", (e) => {
+    window.alert("I did my undergraduate studies in Accountancy!")
+})
+
+document.querySelector("#check-availability").addEventListener("click", (e) => {
+    window.alert("Sorry, I'm fully booked this week!")
+})
+
